dea1962b3162f2383bffcbac9f1360c43fad4c21
Daniel Larsen <danny.r.larsen@gmail.com>
2016-10-19 12:32:25 -0500

    Updated Readme and Added first class

---
+++ src/jText/JText.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jText;
+
+/**
+ * @authors Daniel Larsen, Thomas Nanney, Jacob Lahav,
+ *
+ */
+public class JText {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

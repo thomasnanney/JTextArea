e2d839023e5a37e8502c6f77687722f7091b2e97
Josebeto <jbocanegra96@gmail.com>
2016-11-25 22:39:19 -0600

    Added functionality to paragraph alignment.

--- src/jText/JTextParagraph.java
+++ src/jText/JTextParagraph.java
@@ -3,20 +3,18 @@ package jText;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
+@SuppressWarnings("serial")
 public class JTextParagraph extends JFrame{
 
 	public JTextParagraph(JTextView view) {
 			super("Paragraph");
 			JPanel panelNorth = new JPanel();
 			add(panelNorth, BorderLayout.NORTH);
-			
 			
 			panelNorth.setLayout(new GridLayout(1, 1));
 			JLabel alignmentLabel = new JLabel("Alignment:");
@@ -25,10 +23,6 @@ public class JTextParagraph extends JFrame{
 			String[] alignments = { " ", "Left", "Center", "Right" };
 			JComboBox<String> alignment = new JComboBox<String>(alignments);
 			panelNorth.add(alignment);
-			
-			
-			
-			
 			
 			JTextParagraphHandler handler = new JTextParagraphHandler(view);
 			alignment.addActionListener(handler);
--- src/jText/JTextParagraphHandler.java
+++ src/jText/JTextParagraphHandler.java
@@ -2,19 +2,36 @@ package jText;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JComboBox;
 import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 public class JTextParagraphHandler implements ActionListener{
 	private JTextPane textPane;
 	
 	public JTextParagraphHandler(JTextView view){
 		this.textPane = view.getPane();
-		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		JComboBox<?> cb = (JComboBox<?>)e.getSource();
+		String para = (String)cb.getSelectedItem();
+		StyledDocument doc = textPane.getStyledDocument();
+		SimpleAttributeSet newSet = new SimpleAttributeSet();
+		switch(para){
+			case "Left":
+				StyleConstants.setAlignment(newSet, StyleConstants.ALIGN_LEFT);
+				break;
+			case "Center":
+				StyleConstants.setAlignment(newSet, StyleConstants.ALIGN_CENTER);
+				break;
+			case "Right":
+				StyleConstants.setAlignment(newSet, StyleConstants.ALIGN_RIGHT);
+				break;
+		}
+		doc.setParagraphAttributes(0, doc.getLength(), newSet, false);
 	}
 }

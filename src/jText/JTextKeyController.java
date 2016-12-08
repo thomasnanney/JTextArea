37f55f743adc13b32e1618ebef1b8cceb0ad803a
CJB <CJB@CJBHP.gpmlife.com>
2016-12-08 15:59:01 -0600

    Resetting merge

--- src/jText/JTextKeyController.java
+++ src/jText/JTextKeyController.java
@@ -25,53 +25,6 @@ public class JTextKeyController {
 		textPane = view.getPane();
 		System.out.println(" ");
 		textPane.addKeyListener(new java.awt.event.KeyAdapter() {
-			public void keyReleased(java.awt.event.KeyEvent event){
-				if(event.getKeyCode() == KeyEvent.VK_SLASH && 
-						prev == "/"){
-					//System.out.println("HERE");
-					isComment = 1;
-					//ArrayList<String> commentLine = new ArrayList();
-					
-					
-				}
-				System.out.print(" ");
-				//System.out.println(event.getKeyText(event.getKeyCode()));
-				//evaluate on newline
-				if(event.getKeyCode() == KeyEvent.VK_ENTER && isComment > 0){
-					//System.out.println("HERE");
-					int length = textPane.getText().length();
-					int startOfComment = 0;
-					for(int j = length - 2; j > 0; j--){
-						if(textPane.getText().charAt(j)== '\n'){
-							//System.out.println("HERE");
-							startOfComment = j+1;
-							break;
-						}
-					}
-					textPane.setSelectionStart(startOfComment);
-					textPane.setSelectionEnd(textPane.getCaretPosition());
-					JTextSpecial special = new JTextSpecial(textPane.getSelectedText(), "//");
-					
-				}
-				if(event.getKeyCode() == KeyEvent.VK_SPACE){
-					
-					//System.out.println(event.getKeyText(event.getKeyCode()));
-				}
-	        	if(event.isShiftDown() && event.getKeyCode() == KeyEvent.VK_CLOSE_BRACKET){
-	        		//this only works 100% of the time if the below print statement is included
-	        		//not sure why
-	        		System.out.print(" ");
-	        		int caret = textPane.getCaretPosition();
-	        		textPane.setSelectionStart(0);
-	        		textPane.setSelectionEnd(caret);
-	        		String str = textPane.getSelectedText();
-	        		JTextSpecial special = new JTextSpecial(str, "}");
-	        		String ret = special.getSubString();
-	        		textPane.replaceSelection(str+ret);
-	        	}
-	        	prev = event.getKeyText(event.getKeyCode());
-	        	
-			}
 	        public void keyPressed(java.awt.event.KeyEvent event) {
 	            if (event.isControlDown() && event.getKeyCode() == KeyEvent.VK_S) {
 	                ActionEvent e = new ActionEvent(controller, 0, "Save");
